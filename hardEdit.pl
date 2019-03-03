1a44d21140b2f62db669d38d8fdf388e564748d2
emsjustin <lucas.ems@hotmail.com>
2019-03-03 15:00:15 -0300

    Update hardEdit.pl

--- hardEdit.pl
+++ hardEdit.pl
@@ -4,10 +4,7 @@
 #|  __/ | | | | \__ \| | |_| \__ \ |_| | | | |
 # \___|_| |_| |_|___// |\__,_|___/\__|_|_| |_|
 #                  |__/                       
-#	BECAUSE FARM IS WHAT MATTERS
-# 1 - Criar um arquivo "chomp.txt" dentro da pasta plugins
-# 2 - O conteúdo do arquivo "chomp.txt" será adicionado (ao final do arquivo "config.txt") toda vez que o oepnkore iniciar.
-#     DELETE O CHOMP.TXT após usar, para não adicionar várias linhas iguais.
+#	BECAUSE FARM IS WHAT MATTERS 
 
 package hardEdit;
 
@@ -19,38 +16,54 @@ use Utils;
 use Misc;
 use Log qw(message);
 use vars qw($filecontent $total);
-use Settings::getConfigFilename;
-use File::Spec;
+use Settings;
 
 Plugins::register("hardEdit", "easily add lines at the end of config.txt", \&unload);
 
 
 my $base_hooks = Plugins::addHooks(
-	['start2',  \&edit]
+	['initialized',  \&edit]
+	);
+	
+my $commands_handle = Commands::register(
+	['clear chomp','clear the chomp.txt', \&clear]
 	);
 	
 sub unload {
-	Plugins::delHooks($hooks);
+	Plugins::delHooks($base_hooks);	
 	message "hardEdit plugin reloading or unloading\n", 'success'
 }
 
-our $folder = $Plugins::current_plugin_folder;
+my $file1 = './plugins/chomp.txt';
+my $file2 = Settings::getConfigFilename();
 
 sub edit {
-
-	my $file1 = File::Spec->catdir($folder,'chomp.txt');
-	my $file2 = Settings::getControlFilename($file)
-	
 	open(FILE1, $file1) || die "couldn't open the file!";
-	open(FILE2, $file2) || die "couldn't open the file!";
+	open(FILE2, '>>', $file2) || die "couldn't open the file!";
 	
-	while($filecontent = <FILE1>){
-		if ( $filecontent ) {
-		print FILE2 $filecontent;
-		print "Content add $filecontent";
-    }  
-  } 
-close (FILE1); 
-close (FILE2);
+	if ((-s $file1) > (-z $file1)) {
+	
+		while($filecontent = <FILE1>){
+			if ( $filecontent ) {
+				print FILE2 $filecontent;
+				print "Content add $filecontent";
+				}
+			} 
+			close (FILE1); 
+			close (FILE2);	
+			unload('hardEdit');			
+	} else {
+		unload('hardEdit');
+	}
+
+}
+
+sub clear {
+	my (undef, $args) = @_;
+		if ($args eq 'clear chomp') {
+			open my $tmp, '>', $file1;
+			close ($file1);
+			print " $file1 was cleaned.";
+		}
 }
 1;
